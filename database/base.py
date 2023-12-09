40d48015a8103b410cf9352bd953bdf21143706c
Bartosz Michalski <112180048+bvrtek-dev-py@users.noreply.github.com>
2023-12-09 15:53:54 +0100

    fix(models): add Base class to declarative_base (#10)

--- database/base.py
+++ database/base.py
@@ -1,5 +1,16 @@
-from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
+
+from sqlalchemy import Integer, DateTime, func
+from sqlalchemy.orm import MappedColumn, mapped_column
+from sqlalchemy.orm import declarative_base
 
 
-class Base(DeclarativeBase):
-    pass
+class Base:
+    id: MappedColumn[int] = mapped_column(Integer, primary_key=True, index=True)
+    created_at: MappedColumn[datetime] = mapped_column(DateTime, default=func.now())
+    updated_at: MappedColumn[datetime] = mapped_column(
+        DateTime, default=func.now(), onupdate=func.now()
+    )
+
+
+BaseModel = declarative_base(cls=Base)
