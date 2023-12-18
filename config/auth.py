c70265633ea0110482fa2907132d01f09dd53ab8
Bartosz Michalski <bartosz.michalski777@gmail.com>
2023-12-18 22:01:20 +0100

    fix(user): fix github action

--- config/auth.py
+++ config/auth.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ALGORITHM: str = os.getenv("ALGORITHM")  # type: ignore
-ACCESS_TOKEN_EXPIRE_MINUTES: float = float(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))  # type: ignore
+ACCESS_TOKEN_EXPIRE_MINUTES: float = float(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"), 30)  # type: ignore
 LOGIN_URL: str = "/api/v1/auth/login"
 REFRESH_TOKEN_SECRET_KEY: str = os.getenv("REFRESH_TOKEN_SECRET_KEY")  # type: ignore
 SECRET_KEY: str = os.getenv("SECRET_KEY")  # type: ignore
