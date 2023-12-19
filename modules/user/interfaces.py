bf4129174d6a4557bdeef33322182eb650baeb8b
Bartosz Michalski <bartosz.michalski777@gmail.com>
2023-12-19 09:01:56 +0100

    refactor(user): add repository interface

---
+++ modules/user/interfaces.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+from typing import Sequence
+
+from backend.modules.user.models import User
+
+
+class UserRepositoryInterface(ABC):
+    @abstractmethod
+    async def save(self, user: User) -> User:
+        pass
+
+    @abstractmethod
+    async def update(self, user: User) -> User:
+        pass
+
+    @abstractmethod
+    async def delete(self, user: User) -> None:
+        pass
+
+    @abstractmethod
+    async def get_all(self) -> Sequence[User]:
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, user_id: int) -> User | None:
+        pass
+
+    @abstractmethod
+    async def get_by_email(self, email: str) -> User | None:
+        pass
+
+    @abstractmethod
+    async def get_by_username(self, username: str) -> User | None:
+        pass
