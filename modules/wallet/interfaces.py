09ef8e9af6e5a97d5af508a8c5a1f1035286f5c7
Bartosz Michalski <bartosz.michalski777@gmail.com>
2023-12-19 12:21:09 +0100

    refactor(wallet): add abstractmethod decorators

--- modules/wallet/interfaces.py
+++ modules/wallet/interfaces.py
@@ -1,24 +1,30 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import Sequence
 
 from backend.modules.wallet.models import Wallet
 
 
 class WalletRepositoryInterface(ABC):
+    @abstractmethod
     async def save(self, wallet: Wallet) -> Wallet:
         pass
 
+    @abstractmethod
     async def update(self, wallet: Wallet) -> Wallet:
         pass
 
+    @abstractmethod
     async def delete(self, wallet: Wallet) -> None:
         pass
 
+    @abstractmethod
     async def get_by_user_id(self, user_id: int) -> Sequence[Wallet]:
         pass
 
+    @abstractmethod
     async def get_by_id(self, wallet_id: int) -> Wallet | None:
         pass
 
+    @abstractmethod
     async def get_by_user_id_and_name(self, user_id: int, name: str) -> Wallet | None:
         pass
