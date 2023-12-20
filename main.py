01c3fb28976caf8ac49ee3d2b49421fd38b44d89
Bartosz Michalski <112180048+bvrtek-dev-py@users.noreply.github.com>
2023-12-20 09:36:00 +0100

    feat(database): add alembic migrations (#38)

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import asyncio
-
 import uvicorn
 from fastapi import FastAPI
 
@@ -9,12 +7,6 @@ from backend.api.v1.subject.routers import router as subject_router
 from backend.api.v1.transaction.routers import router as transaction_router
 from backend.api.v1.user.routers import router as user_router
 from backend.api.v1.wallet.routers import router as wallet_router
-from backend.database.setup import async_engine_factory
-from backend.modules.category.models import Category
-from backend.modules.subject.models import Subject
-from backend.modules.transaction.models import Transaction
-from backend.modules.user.models import User
-from backend.modules.wallet.models import Wallet
 
 app = FastAPI()
 
@@ -26,16 +18,5 @@ app.include_router(subject_router)
 app.include_router(auth_router)
 
 
-async def create_tables() -> None:
-    # Will be deleted (when add alembic)
-    async with async_engine_factory().begin() as engine:
-        await engine.run_sync(User.metadata.create_all)
-        await engine.run_sync(Wallet.metadata.create_all)
-        await engine.run_sync(Category.metadata.create_all)
-        await engine.run_sync(Transaction.metadata.create_all)
-        await engine.run_sync(Subject.metadata.create_all)
-
-
 if __name__ == "__main__":
-    asyncio.run(create_tables())
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
