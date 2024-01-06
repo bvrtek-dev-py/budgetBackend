dccdabf6986dabb995bf4decf1624d55d45cab6a
Bartosz Michalski <bartosz.michalski777@gmail.com>
2024-01-06 18:56:17 +0100

    feat(user): fix mypy error

--- modules/transaction/filters.py
+++ modules/transaction/filters.py
@@ -1,4 +1,5 @@
 from datetime import date
+from decimal import Decimal
 from typing import Optional
 
 from sqlalchemy import Select
@@ -7,10 +8,10 @@ from backend.modules.transaction.models import Transaction
 
 
 def filter_query_by_date_range(
-    query: Select[tuple[Transaction]],
+    query: Select,
     start_date: Optional[date] = None,
     end_date: Optional[date] = None,
-) -> Select[tuple[Transaction]]:
+) -> Select:
     if start_date is not None:
         query = query.where(Transaction.date >= start_date)
 
