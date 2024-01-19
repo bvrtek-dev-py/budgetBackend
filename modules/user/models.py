54e39daaab6a3edf4d490b630d13a9f3e47d5d67
Bartosz Michalski <bartosz.michalski777@gmail.com>
2024-01-19 16:01:55 +0100

    feature(user): add user admin role

--- modules/user/models.py
+++ modules/user/models.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from sqlalchemy import String
+from sqlalchemy import String, Boolean
 from sqlalchemy.orm import MappedColumn, mapped_column, relationship
 
 from backend.database.base import BaseModel
@@ -14,6 +14,8 @@ class User(BaseModel):
     username: MappedColumn[str] = mapped_column(String(255), unique=True, index=True)
     email: MappedColumn[str] = mapped_column(String(255), unique=True)
     password: MappedColumn[str] = mapped_column(String(255))
+    is_admin: MappedColumn[bool] = mapped_column(Boolean, default=False)
+
     wallets: MappedColumn[List["Wallet"]] = relationship(back_populates="user")  # type: ignore
     categories: MappedColumn[List["Category"]] = relationship(back_populates="user")  # type: ignore
     subjects: MappedColumn[List["Subject"]] = relationship(back_populates="user")  # type: ignore
