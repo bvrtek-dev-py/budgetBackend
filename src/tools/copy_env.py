fc8fcc439d9918ca5c6b6b45e654dce9aa98957b
Bartosz Michalski <bartosz.michalski777@gmail.com>
2024-01-20 15:27:36 +0100

    fix

--- src/tools/copy_env.py
+++ src/tools/copy_env.py
@@ -1,6 +1,6 @@
 import os
 import shutil
 
-BACKEND_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+BACKEND_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 
-shutil.copy(f"{BACKEND_DIR}/config/.env.example", f"{BACKEND_DIR}/config/.env")
+shutil.copy(f"{BACKEND_DIR}/src/config/.env.example", f"{BACKEND_DIR}/src/config/.env")
