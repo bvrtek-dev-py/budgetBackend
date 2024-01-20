7a92fd5c9f5af7419de139b91e4e7e00615f13f6
Bartosz Michalski <bartosz.michalski777@gmail.com>
2024-01-20 22:29:05 +0100

    fix(auth): fix pylint

--- src/api/v1/user/requests.py
+++ src/api/v1/user/requests.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, ValidationError, model_validator
+from pydantic import BaseModel, Field, model_validator
 
 from backend.src.core.modules.user.exceptions import PasswordDoesNotMatch
 
