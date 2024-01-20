f4bcd4b2332629a63cfdd6fa045e106dd4164a3d
Bartosz Michalski <112180048+bvrtek-dev-py@users.noreply.github.com>
2024-01-21 00:11:22 +0100

    refactor(auth): move logic from refresh token endpoint (#85)

--- src/api/v1/auth/routers.py
+++ src/api/v1/auth/routers.py
@@ -3,13 +3,12 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
 
-from backend.src.api.v1.auth.repsonses import TokenResponse
+from backend.src.api.v1.auth.repsonses import LoginSuccessResponse
 from backend.src.api.v1.auth.requests import PasswordChangeRequest
 from backend.src.api.v1.common.responses import ErrorResponse
 from backend.src.api.v1.user.responses import UserBaseResponse
 from backend.src.config.oauth2 import oauth2_scheme
 from backend.src.core.modules.auth.schemas import (
-    AuthenticatedUserDTO,
     CurrentUserData,
     ChangePasswordDTO,
 )
@@ -17,15 +16,17 @@ from backend.src.core.modules.auth.services.login_service import LoginService
 from backend.src.core.modules.auth.services.password_services import (
     PasswordChangeService,
 )
-from backend.src.core.modules.auth.services.token_service import TokenService
 from backend.src.dependencies.auth.creators import (
-    get_token_service,
+    get_refresh_token_service,
 )
+from backend.src.dependencies.auth.permissions import get_current_user
 from backend.src.dependencies.auth.providers import (
     get_password_change_service,
     get_login_service,
 )
-from backend.src.dependencies.auth.permissions import get_current_user
+from backend.src.core.modules.auth.services.refresh_token_service import (
+    RefreshTokenService,
+)
 
 router = APIRouter(prefix="/api/v1/auth", tags=["APIv1 Auth"])
 
@@ -33,10 +34,12 @@ router = APIRouter(prefix="/api/v1/auth", tags=["APIv1 Auth"])
 @router.post(
     "/login",
     responses={
-        200: {"model": TokenResponse},
+        200: {"model": LoginSuccessResponse},
         404: {"model": ErrorResponse},
         409: {"model": ErrorResponse},
     },
+    response_model=LoginSuccessResponse,
+    status_code=200,
 )
 async def login(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
@@ -48,24 +51,20 @@ async def login(
 @router.post(
     "/refresh-token",
     responses={
-        200: {"model": TokenResponse},
+        200: {"model": LoginSuccessResponse},
         401: {"model": ErrorResponse},
         404: {"model": ErrorResponse},
     },
+    response_model=LoginSuccessResponse,
+    status_code=200,
 )
 async def refresh_token(
     token: Annotated[str, Depends(oauth2_scheme)],
-    token_service: Annotated[TokenService, Depends(get_token_service)],
+    refresh_token_service: Annotated[
+        RefreshTokenService, Depends(get_refresh_token_service)
+    ],
 ):
-    decoded_data = token_service.decode(token)
-    token_data = AuthenticatedUserDTO(id=decoded_data.id, sub=decoded_data.email)
-
-    return {
-        "token_type": "Bearer",
-        "access_token": token_service.create_access_token(token_data),
-        "refresh_token": token_service.create_refresh_token(token_data),
-        "expired_at": token_service.get_expire_token_datetime(),
-    }
+    return refresh_token_service.refresh(token)
 
 
 @router.post(
