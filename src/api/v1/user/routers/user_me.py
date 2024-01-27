673ac4686ea674d12585c76aeec00aec029f6adb
Bartosz Michalski <112180048+bvrtek-dev-py@users.noreply.github.com>
2024-01-27 15:24:02 +0100

    feat(user): add user me update endpoint (#89)
    * feat(user): add user me update endpoint
    
    * feat(user): add user me update endpoint

--- src/api/v1/user/routers/user_me.py
+++ src/api/v1/user/routers/user_me.py
@@ -2,6 +2,8 @@ from typing import Annotated, List, Optional
 
 from fastapi import APIRouter, Depends, status
 
+from backend.src.api.v1.user.requests import UserUpdateRequest
+from backend.src.api.v1.user.responses import UserBaseResponse
 from backend.src.api.v1.category.responses import CategoryGetResponse
 from backend.src.api.v1.common.responses import ErrorResponse
 from backend.src.api.v1.subject.responses import SubjectBaseResponse
@@ -13,8 +15,31 @@ from backend.src.core.modules.auth.schemas import CurrentUserDTO
 from backend.src.core.modules.category.service import CategoryService
 from backend.src.core.modules.transaction.enum import TransactionType
 from backend.src.core.modules.user.service import UserService
+from backend.src.core.modules.user.schemas import UserUpdateDTO
 
 router = APIRouter(prefix="/api/v1/users/me", tags=["APIv1 User Me"])
+
+
+@router.put(
+    "",
+    responses={
+        200: {"model": UserBaseResponse},
+        401: {"model": ErrorResponse},
+        403: {"model": ErrorResponse},
+        404: {"model": ErrorResponse},
+        409: {"model": ErrorResponse},
+    },
+    response_model=UserBaseResponse,
+    status_code=status.HTTP_200_OK,
+)
+async def update_user(
+    current_user: Annotated[CurrentUserDTO, Depends(get_current_user)],
+    request: UserUpdateRequest,
+    user_service: Annotated[UserService, Depends(get_user_service)],
+):
+    return await user_service.update(
+        current_user.id, UserUpdateDTO(**request.model_dump())
+    )
 
 
 @router.get(
